121f39c7332fe58ec569b43fd62e67ef3d7d8d5b
Ka Kyen Lam <s3524228@student.rmit.edu.au>
2017-03-14 14:58:17 +1100

    Fixed error (default field)

--- src/model/Card.java
+++ src/model/Card.java
@@ -7,7 +7,7 @@ public class Card {
     private boolean discarded;
 
     public static void main(String[] args) {
-        System.out.println("teting");
+        System.out.println("testing");
     }
 
     public static void cardAction() {
