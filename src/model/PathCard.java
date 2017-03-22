8cae9e4ea1137b184a73d46ac7b135980cfb109a
rmit-s3585826-fabio-monsalve <s3585826@rmit.edu.au>
2017-03-22 21:47:09 +1100

    Updated rotate logic

--- src/model/PathCard.java
+++ src/model/PathCard.java
@@ -8,25 +8,42 @@ public class PathCard extends Card {
     private boolean east;
     private boolean centre;
 
+    public static void main(String args[]){
+        PathCard pCard = new PathCard(true, true, true, false);
+        pCard.printCurrentDirections();
+        pCard.rotate("right");
+        pCard.printCurrentDirections();
+    }
+    public PathCard (boolean north, boolean west, boolean south, boolean east){
+        this.north = north;
+        this.west = west;
+        this.south = south;
+        this.east = east;
+    }
+
     private void rotate(String direction) {
         boolean tempNorth = north;
-        boolean tempSouth = south;
         boolean tempWest = west;
+        boolean tempSouth = south;
         boolean tempEast = east;
-        boolean temp [] = {tempNorth, tempSouth, tempEast, tempWest};
+        boolean temp [] = {tempNorth, tempWest, tempSouth, tempEast};
 
         if(direction.equals("right")) {
             if (temp[0]) {
                 east = true;
+                north = false;
             }
             if (temp[1]) {
-                west = true;
+                north = true;
+                west = false;
             }
             if (temp[2]) {
-                south = true;
+                west = true;
+                south = false;
             }
             if (temp[3]) {
-                north = true;
+                south = true;
+                east = false;
             }
         }
 
@@ -44,6 +61,11 @@ public class PathCard extends Card {
                 north = true;
             }
         }
+    }
+
+    private void printCurrentDirections(){
+        System.out.println("North: " + north + "\n" + "West: " + west + "\n" + "South: " + south + "\n" +
+                "East: " + east + "\n");
     }
 
     private boolean updateIsValid() {
