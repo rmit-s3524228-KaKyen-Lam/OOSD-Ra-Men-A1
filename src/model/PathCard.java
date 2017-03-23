376909ab53132134daf9396185d6c2f8d7a660e5
rmit-s3585826-fabio-monsalve <s3585826@student.rmit.edu.au>
2017-03-23 16:39:25 +1100

    Edited PathCard constructor

--- src/model/PathCard.java
+++ src/model/PathCard.java
@@ -1,24 +1,71 @@
 package model;
 
 public class PathCard extends Card {
-    private boolean isValid;
-    private boolean north;
-    private boolean west;
-    private boolean south;
-    private boolean east;
-    private boolean centre;
+    private boolean isValid = false;
+    private boolean north = false;
+    private boolean west = false;
+    private boolean south = false;
+    private boolean east = false;
+    private boolean centre = false;
+    public final static int T_SHAPE = 1;
+    public final static int T_SHAPE_DEAD = 2;
+    public final static int LINE_SHAPE = 3;
+    public final static int LINE_SHAPE_DEAD = 4;
+    public final static int CROSS_SHAPE = 5;
+    public final static int CROSS_SHAPE_DEAD = 6;
+    public final static int L_SHAPE = 7;
+    public final static int L_SHAPE_DEAD = 8;
+    public final static int DEAD = 9;
+    public final static int EMPTY = 10;
 
     public static void main(String args[]){
-        PathCard pCard = new PathCard(true, true, true, false);
+        PathCard pCard = new PathCard(1);
         pCard.printCurrentDirections();
         pCard.rotate("right");
         pCard.printCurrentDirections();
     }
-    public PathCard (boolean north, boolean west, boolean south, boolean east){
-        this.north = north;
-        this.west = west;
-        this.south = south;
-        this.east = east;
+
+    public PathCard (int configNo){
+        switch (configNo){
+            case T_SHAPE: south = true;
+                west = true;
+                east = true;
+                centre = true;
+            case T_SHAPE_DEAD: south = true;
+                west = true;
+                east = true;
+                break;
+            case LINE_SHAPE: west = true;
+                east = true;
+                centre = false;
+                break;
+            case LINE_SHAPE_DEAD: west = true;
+                east = true;
+                break;
+            case CROSS_SHAPE: north = true;
+                west = true;
+                east = true;
+                south = true;
+                centre = true;
+                break;
+            case CROSS_SHAPE_DEAD: north = true;
+                west = true;
+                east = true;
+                south = true;
+                break;
+            case L_SHAPE: west = true;
+                south = true;
+                centre = true;
+                break;
+            case L_SHAPE_DEAD: north = true;
+                south = true;
+                break;
+            case DEAD: north = true;
+                centre = false;
+                break;
+            case EMPTY:
+                break;
+        }
     }
 
     private void rotate(String direction) {
@@ -28,7 +75,8 @@ public class PathCard extends Card {
         boolean tempEast = east;
         boolean temp [] = {tempNorth, tempWest, tempSouth, tempEast};
 
-        if(direction.equals("right")) {
+        if(direction.equals("cw")) {
+
             if (temp[0]) {
                 east = true;
                 north = false;
@@ -47,7 +95,7 @@ public class PathCard extends Card {
             }
         }
 
-        if(direction.equals("left")){
+        if(direction.equals("acw")){
             if (temp[0]) {
                 west = true;
             }
@@ -68,7 +116,32 @@ public class PathCard extends Card {
                 "East: " + east + "\n");
     }
 
-    private boolean updateIsValid() {
-        return true;
+    public void setValid(boolean valid) {
+        isValid = valid;
     }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public boolean isNorth() {
+        return north;
+    }
+
+    public boolean isWest() {
+        return west;
+    }
+
+    public boolean isSouth() {
+        return south;
+    }
+
+    public boolean isEast() {
+        return east;
+    }
+
+    public boolean isCentre() {
+        return centre;
+    }
+
 }
