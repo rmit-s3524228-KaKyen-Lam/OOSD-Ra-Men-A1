c6c6bc0376afdc8d01af3f41cb90ce4baf272540
Ka Kyen Lam <s3524228@student.rmit.edu.au>
2017-04-02 17:37:51 +1000

    Added randomPosition

--- src/model/Board.java
+++ src/model/Board.java
@@ -5,7 +5,7 @@ import java.util.*;
 import static model.GoalCard.*;
 
 /*http://stackoverflow.com/questions/9369368/2d-arraylist-in-java
- * */
+ https://www.thoughtco.com/generating-unique-random-numbers-2034208*/
 
 /**
  * Board of game
@@ -14,28 +14,36 @@ import static model.GoalCard.*;
  */
 public class Board {
 
-    public static final int GRID_MAX_WIDTH = 9;
-    public static final int GRID_MAX_HEIGHT = 7;
-    public static final int START_GOAL_X = 8;
-    public static int COAL_ONE_Y = 1;
-    public static int COAL_TWO_Y = 3;
-    public static int GOLD_Y = 5;
-    public static final int START_X = 0;
-    public static final int START_Y = 3;
+    private static final int GRID_MAX_WIDTH = 9;
+    private static final int GRID_MAX_HEIGHT = 7;
+    private static final int START_GOAL_X = 8;
+    private static ArrayList<Integer> START_GOAL_Y = new ArrayList<>();
+    private static int GOAL_ONE = 1;
+    private static int GOAL_TWO = 3;
+    private static int GOAL_THREE = 5;
+    private static final int START_X = 0;
+    private static final int START_Y = 3;
 
 
     private Grid[][] grid = new Grid[GRID_MAX_WIDTH][GRID_MAX_HEIGHT];
     private Grid goldLocation;
 
+    public void randomPosition() {
+        START_GOAL_Y.add(GOAL_ONE);
+        START_GOAL_Y.add(GOAL_TWO);
+        START_GOAL_Y.add(GOAL_THREE);
+
+        Collections.shuffle(START_GOAL_Y);
+    }
 
     public void initBoard() {
-
+        randomPosition();
         for (int i = 0; i < GRID_MAX_HEIGHT; i++) {
             for (int j = 0; j < GRID_MAX_WIDTH; j++) {
-                if ((i == COAL_ONE_Y || i == COAL_TWO_Y) && j == START_GOAL_X) {
+                if ((i == START_GOAL_Y.get(1) || i == START_GOAL_Y.get(2)) && j == START_GOAL_X) {
                     grid[j][i] = new Grid(j, i, new GoalCard("resources/Goal.png", "resources/Coal.png", "coal"));
-                } else if (i == GOLD_Y && j == START_GOAL_X) {
-                    grid[j][i] = new Grid(j, i, new GoalCard("resources/Goal.png", "resources/Coal.png", "gold"));
+                } else if (i == START_GOAL_Y.get(3) && j == START_GOAL_X) {
+                    grid[j][i] = new Grid(j, i, new GoalCard("resources/Goal.png", "resources/Gold.png", "gold"));
                     goldLocation = grid[j][i];
                 } else if (i == START_Y && j == START_X) {
                     grid[j][i] = new Grid(j, i, new PathCard(PathCard.CROSS_SHAPE, "resources/Shape_Plus.png", "cross") );
