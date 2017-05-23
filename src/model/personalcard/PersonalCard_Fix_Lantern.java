5860af3bae0ae218d45af3b95360b62f70345152
Ka Kyen Lam <lamkk91@gmail.com>
2017-05-23 18:22:22 +1000

    Created initial personal cards

---
+++ src/model/personalcard/PersonalCard_Fix_Lantern.java
@@ -0,0 +1,31 @@
+package model.personalcard;
+
+import model.PersonalCard;
+
+/**
+ * Created by Ka Kyen Lam on 23/05/2017.
+ */
+public class PersonalCard_Fix_Lantern extends PersonalCard {
+    /**
+     * @param imageResource location of image file for card
+     * @param id
+     */
+    public PersonalCard_Fix_Lantern(String imageResource, String id) {
+        super(imageResource, id);
+    }
+
+    /**
+     * Method for actions different types of cards might have. E.g. Breaking tools
+     *
+     * @param target
+     */
+    @Override
+    public boolean cardAction(Object[] target) {
+        return false;
+    }
+
+    @Override
+    public void undoCardAction(Object[] target, Object[] undoExtraInformation) {
+
+    }
+}
